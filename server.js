1cefc19ad2a46ef8cdd8ed94d9c7ac4cbbcc4e6b
Yogesh Malkani <yogesh.malkani@pillup.com>
2024-04-04 22:14:27 +0530

    cahmnd

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
